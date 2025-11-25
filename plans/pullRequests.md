f8e810051be69815e9309b2e2709cdb9ca1b90c7
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-11-24 17:51:27 -0800

    updated pr list format, revised javadoc

--- plans/pullRequests.md
+++ plans/pullRequests.md
@@ -23,14 +23,17 @@
 ## Improving Error Handling
 
 - improve exception handling video processing app
+
   - https://github.com/mlarsen-source/centroid-finder/pull/2
 
 ## Adding Tests
 
 - improve/add tests (processor and server)
+
   - https://github.com/mlarsen-source/centroid-finder/pull/5/files
 
 ## Refactoring Code
 
 - implement interfaces for VideoProcessor, VideoGroup Finder, CsvWriter, CommandLineParser
+
   - https://github.com/mlarsen-source/centroid-finder/pull/3
--- processor/src/main/java/io/github/mlarsen_source/centroid_finder/BinaryGroupFinder.java
+++ processor/src/main/java/io/github/mlarsen_source/centroid_finder/BinaryGroupFinder.java
@@ -12,7 +12,7 @@ public interface BinaryGroupFinder {
     * is thrown. If the array is otherwise invalid, an IllegalArgumentException
     * is thrown.
     *
-    * Pixels are considered connected vertically and horizontally, NOT diagonally.
+    * Pixels are considered connected vertically and horizontally, not diagonally.
     * The top-left cell of the array (row:0, column:0) is considered to be coordinate
     * (x:0, y:0). Y increases downward and X increases to the right. For example,
     * (row:4, column:7) corresponds to (x:7, y:4).
@@ -20,13 +20,8 @@ public interface BinaryGroupFinder {
     * The method returns a list of sorted groups. The group's size is the number 
     * of pixels in the group. The centroid of the group
     * is computed as the average of each of the pixel locations across each dimension.
-    * For example, the x coordinate of the centroid is the sum of all the x
-    * coordinates of the pixels in the group divided by the number of pixels in that group.
-    * Similarly, the y coordinate of the centroid is the sum of all the y
-    * coordinates of the pixels in the group divided by the number of pixels in that group.
-    * The division should be done as INTEGER DIVISION.
     *
-    * The groups are sorted in DESCENDING order according to Group's compareTo method
+    * The groups are sorted in descending order according to Group's compareTo method
     * (size first, then x, then y). That is, the largest group will be first, the 
     * smallest group will be last, and ties will be broken first by descending 
     * y value, then descending x value.
